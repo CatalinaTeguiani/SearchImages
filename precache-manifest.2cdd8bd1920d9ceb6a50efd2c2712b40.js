8ee52db319e634f6bbcee179051cd11ab080637d
Cata001 <catalinateguiani@yahoo.com>
2019-07-25 15:26:20 +0100

    Updates

---
+++ precache-manifest.2cdd8bd1920d9ceb6a50efd2c2712b40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c0273480c2648518d023bb7476e258",
+    "url": "/SearchImages/index.html"
+  },
+  {
+    "revision": "a7b4b33117bd5cdcdf4b",
+    "url": "/SearchImages/static/css/main.b20bc3ef.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/SearchImages/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "a7b4b33117bd5cdcdf4b",
+    "url": "/SearchImages/static/js/main.3475802e.chunk.js"
+  },
+  {
+    "revision": "cef4dc08639a7a1e9de3",
+    "url": "/SearchImages/static/js/runtime~main.f369fb07.js"
+  }
+]);
